5562c9bb7061e2a0d4483b7498bbacfcb9b8c7c2
M.P.C PRIYAMANTHA <95483538+ChandimalPriyamantha@users.noreply.github.com>
2023-03-21 15:38:09 +0530

    This is a beginner of flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,16 @@
 import 'package:flutter/material.dart';
-import 'question.dart';
-import 'answer.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  var _questionIndex = 0;
-  void _answerQuestion() {
-    setState(() {
-      _questionIndex = _questionIndex + 1;
-    });
-
-    print(_questionIndex);
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var question = [
-      'What\'s your favorite career?',
-      'What\'s your degree program?'
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("This is not my first App"),
+          title: Text('My First App'),
         ),
-        body: Column(
-          children: [
-            Question(question[_questionIndex]),
-            Answwer(_answerQuestion),
-            Answwer(_answerQuestion),
-            Answwer(_answerQuestion),
-          ],
-        ),
+        body: Text('Tis is my defual text'),
       ),
     );
   }
